6081f84cdfe03547c383a50acbbdb603c63fbfd3
bdynamite <bdynamite@yandex.ru>
2017-08-01 13:41:45 +0300

    переназвал кортеж, изменил имена

--- coursera.py
+++ coursera.py
@@ -9,25 +9,22 @@ import openpyxl
 from openpyxl.styles import Alignment
 import tqdm
 
-
-URL = 'https://www.coursera.org/sitemap~www~courses.xml'
-COURSES_QUANTITY = 20
-FILEPATH = 'coursera.xlsx'
-
-Course = collections.namedtuple('Course', 'name lang start weeks stars')
+course = collections.namedtuple('Course', 'name lang start weeks stars')
 
 
 def get_courses_list():
-    responce = requests.get(url=URL)
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    quantity = 20
+    responce = requests.get(url=url)
     root = ET.fromstring(responce.text)
     courses = [child[0].text for child in root]
-    return random.sample(courses, COURSES_QUANTITY)
+    return random.sample(courses, quantity)
 
 
 def get_course_info(course_slug):
     responce = requests.get(url=course_slug)
     soup = BeautifulSoup(responce.text, 'html.parser')
-    name = soup.title.text.split('|')[0]
+    course_name = soup.title.text.split('|')[0]
     lang = soup.find_all(attrs={'class': 'rc-Language'})[0].text
     start = soup.find_all(attrs={'class': 'startdate rc-StartDateString caption-text'})[0].text.split()[1:]
     start = ' '.join(start)
@@ -36,10 +33,11 @@ def get_course_info(course_slug):
         stars = soup.find_all(attrs={'class': 'ratings-text headline-2-text'})[0].text.split()[1]
     except IndexError:
         stars = None
-    return Course(name=name, lang=lang, start=start, weeks=weeks, stars=stars)
+    return course(name=course_name, lang=lang, start=start, weeks=weeks, stars=stars)
 
 
 def output_courses_info_to_xlsx(courses):
+    filepath = 'coursera.xlsx'
     wb = openpyxl.Workbook()
     ws = wb.active
     ws.title = 'courses'
@@ -51,7 +49,8 @@ def output_courses_info_to_xlsx(courses):
             ws[cell] = element
             if cell[0] in 'AB':
                 ws[cell].alignment = Alignment(wrapText=True)
-    wb.save(filename=FILEPATH)
+    wb.save(filename=filepath)
+    return filepath
 
 
 def create_xlsx_header(sheet):
@@ -64,13 +63,13 @@ def create_xlsx_header(sheet):
     sheet['E1'] = 'Rating'
 
 
-def output_path():
+def output_path(filepath):
     print('Courses saved at')
-    print(os.path.join(os.getcwd(), FILEPATH))
+    print(os.path.join(os.getcwd(), filepath))
     
 
 if __name__ == '__main__':
     courses = get_courses_list()
     courses_info = list(map(get_course_info, tqdm.tqdm(courses, desc='collecting data')))
-    output_courses_info_to_xlsx(courses_info)
-    output_path()
+    filepath = output_courses_info_to_xlsx(courses_info)
+    output_path(filepath)
